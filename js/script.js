e4579170db7fa22e8c361dd4dbc577e3f652fe88
Feycalion <anjakves@gmail.com>
2023-11-20 23:32:19 +0100

    add functional load more, add newsletter section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-const url = "http://flower-power.local/wp-json/wp/v2/posts?per_page=20";
-const categoriesUrl = "http://flower-power.local/wp-json/wp/v2/categories";
+// const url = "http://flower-power.local/wp-json/wp/v2/posts?per_page=20";
+// const categoriesUrl = "http://flower-power.local/wp-json/wp/v2/categories";
+
+let offsetValue = 0;
+let postLimit = 9;
 
 async function getData() {
+  const url = `http://anjakvernenes.no/wp-json/wp/v2/posts?per_page=${postLimit}&offset=${offsetValue}`;
+  console.log(url);
+
+  const categoriesUrl = "http://anjakvernenes.no/wp-json/wp/v2/categories";
+
   const [postsResponse, categoriesResponse] = await Promise.all([
     fetch(url),
     fetch(categoriesUrl),
@@ -10,21 +18,44 @@ async function getData() {
     postsResponse.json(),
     categoriesResponse.json(),
   ]);
-
-  console.log(categories);
-
   printData(posts, categories);
+  return [posts, categories];
 }
 
 getData();
 
+/*
+
+async function saveAndPrintData() {
+  const [posts, categories] = await getData();
+  const firstHalf = splitArray(posts).left;
+  const secondHalf = splitArray(posts).right;
+  const specificPost = firstHalf.find((element) => element.id == 110);
+  console.log(specificPost);
+
+  printData(firstHalf, categories);
+}
+
+// split half
+
+function splitArray(posts) {
+  (mid = Math.ceil(posts.length / 2)),
+    (obj = {
+      left: posts.slice(0, mid),
+      right: posts.slice(mid),
+    });
+  return obj;
+}
+
+saveAndPrintData();
+
+*/
 function printData(posts, categories) {
   const dataContainer = document.querySelector(".data-container");
-  dataContainer.innerHTML = "";
 
   for (let i = 0; i < posts.length; i++) {
-    console.log(posts[i]);
-    const container = document.createElement("div");
+    const container = document.createElement("a");
+    container.href = `post.html?id=${posts[i].id}`;
     container.classList.add("container");
 
     dataContainer.appendChild(container);
@@ -57,9 +88,22 @@ function printData(posts, categories) {
     container.appendChild(recipeName);
 
     dataContainer.appendChild(container);
+
+    // readmore
+    const readMore = document.createElement("p");
+    readMore.innerText = "Read more";
+    readMore.classList.add("txtstyle");
+    container.appendChild(readMore);
   }
 }
 
 function getCategoryById(categories, categoryId) {
   return categories.find((category) => category.id === categoryId);
 }
+
+const loadMore = document.querySelector(".load-more-btn");
+
+loadMore.onclick = function () {
+  offsetValue += 9;
+  getData();
+};
